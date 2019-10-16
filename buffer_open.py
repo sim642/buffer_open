893bf434fcb76f5d166d96e65fb1e9a560b614e5
Simmo Saan <simmo.saan@gmail.com>
2019-10-16 22:59:36 +0300

    Add option layout_apply

--- buffer_open.py
+++ buffer_open.py
@@ -52,7 +52,10 @@ import re
 
 
 SETTINGS = {
-
+    "layout_apply": (
+        "off",
+        "open closed layout buffers on /layout apply"
+    )
 }
 
 
@@ -179,26 +182,26 @@ LAYOUT_APPLY_RE = re.compile(r"/layout apply(?:\s+(\S+)(?:\s+buffers)?)?")
 
 
 def layout_apply_cb(data, buffer, command):
-    m = LAYOUT_APPLY_RE.match(command)
-    if m:
-        # TODO: add option for this
-        layout_name = m.group(1) or "default"
-        hdata_layout = weechat.hdata_get("layout")
-        layouts = weechat.hdata_get_list(hdata_layout, "gui_layouts")
-        layout = weechat.hdata_search(hdata_layout, layouts, "${layout.name} == " + layout_name, 1)
-        if layout:
-            hdata_layout_buffer = weechat.hdata_get("layout_buffer")
-            layout_buffer = weechat.hdata_pointer(hdata_layout, layout, "layout_buffers")
-            while layout_buffer:
-                plugin_name = weechat.hdata_string(hdata_layout_buffer, layout_buffer, "plugin_name")
-                buffer_name = weechat.hdata_string(hdata_layout_buffer, layout_buffer, "buffer_name")
-                full_name = "{}.{}".format(plugin_name, buffer_name)
+    if weechat.config_string_to_boolean(weechat.config_get_plugin("layout_apply")):
+        m = LAYOUT_APPLY_RE.match(command)
+        if m:
+            layout_name = m.group(1) or "default"
+            hdata_layout = weechat.hdata_get("layout")
+            layouts = weechat.hdata_get_list(hdata_layout, "gui_layouts")
+            layout = weechat.hdata_search(hdata_layout, layouts, "${layout.name} == " + layout_name, 1)
+            if layout:
+                hdata_layout_buffer = weechat.hdata_get("layout_buffer")
+                layout_buffer = weechat.hdata_pointer(hdata_layout, layout, "layout_buffers")
+                while layout_buffer:
+                    plugin_name = weechat.hdata_string(hdata_layout_buffer, layout_buffer, "plugin_name")
+                    buffer_name = weechat.hdata_string(hdata_layout_buffer, layout_buffer, "buffer_name")
+                    full_name = "{}.{}".format(plugin_name, buffer_name)
 
-                buffer = weechat.buffer_search("==", full_name)
-                if not buffer:
-                    buffer_open_full_name(full_name, noswitch=True)
+                    buffer = weechat.buffer_search("==", full_name)
+                    if not buffer:
+                        buffer_open_full_name(full_name, noswitch=True)
 
-                layout_buffer = weechat.hdata_move(hdata_layout_buffer, layout_buffer, 1)
+                    layout_buffer = weechat.hdata_move(hdata_layout_buffer, layout_buffer, 1)
     return weechat.WEECHAT_RC_OK
 
 
